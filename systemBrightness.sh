65ecc175fba34623a53602b3b2f309c72d677c41
tubbadu <tubbadu@gmail.com>
2022-06-20 16:59:14 +0200

    added systembrightness.sh

---
+++ systemBrightness.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+qdbus-qt5 org.kde.Solid.PowerManagement /org/kde/Solid/PowerManagement/Actions/BrightnessControl org.kde.Solid.PowerManagement.Actions.BrightnessControl.setBrightness $(($(qdbus-qt5 org.kde.Solid.PowerManagement /org/kde/Solid/PowerManagement/Actions/BrightnessControl org.kde.Solid.PowerManagement.Actions.BrightnessControl.brightness) + $1)) 
