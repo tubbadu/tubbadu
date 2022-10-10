a9afc4f301900beb9cd2effacf3ed893ea155e4a
tubbadu <tubbadu@gmail.com>
2022-10-10 19:43:32 +0200

    added powersaver

---
+++ power-saver.sh
@@ -0,0 +1,27 @@
+currentProfile=$(qdbus local.org_kde_powerdevil /org/kde/Solid/PowerManagement/Actions/PowerProfile org.kde.Solid.PowerManagement.Actions.PowerProfile.currentProfile)
+
+if [[ $1 == "scriptinator-status" ]]
+then
+	if [[ $currentProfile == "power-saver" ]]
+	then
+		echo '{PlasmoidIconStart}/home/tubbadu/Immagini/Wallpapers/power-saving-on.svg{PlasmoidIconEnd}'
+	else
+		echo '{PlasmoidIconStart}/home/tubbadu/Immagini/Wallpapers/power-saving-off.svg{PlasmoidIconEnd}'
+	fi
+	exit
+fi
+
+if [[ $currentProfile == "power-saver" ]]
+then
+	echo "current profile: power-saver"
+	echo "setting profile: performance"
+	qdbus local.org_kde_powerdevil /org/kde/Solid/PowerManagement/Actions/PowerProfile org.kde.Solid.PowerManagement.Actions.PowerProfile.setProfile performance
+	qdbus local.org_kde_powerdevil /org/kde/Solid/PowerManagement/Actions/KeyboardBrightnessControl org.kde.Solid.PowerManagement.Actions.KeyboardBrightnessControl.setKeyboardBrightness 1
+	qdbus local.org_kde_powerdevil /org/kde/Solid/PowerManagement/Actions/BrightnessControl org.kde.Solid.PowerManagement.Actions.BrightnessControl.setBrightness 96000
+else
+	echo "current profile: $currentProfile"
+	echo "setting profile: power-saver"
+	qdbus local.org_kde_powerdevil /org/kde/Solid/PowerManagement/Actions/PowerProfile org.kde.Solid.PowerManagement.Actions.PowerProfile.setProfile power-saver
+	qdbus local.org_kde_powerdevil /org/kde/Solid/PowerManagement/Actions/KeyboardBrightnessControl org.kde.Solid.PowerManagement.Actions.KeyboardBrightnessControl.setKeyboardBrightness 0
+	qdbus local.org_kde_powerdevil /org/kde/Solid/PowerManagement/Actions/BrightnessControl org.kde.Solid.PowerManagement.Actions.BrightnessControl.setBrightness 72000
+fi
