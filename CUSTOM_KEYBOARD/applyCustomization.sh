9b92ba9c605372cb4ece8168b8f9451a2b39e14b
tubbadu <tubbadu@gmail.com>
2023-01-05 14:51:03 +0100

    something

--- CUSTOM_KEYBOARD/applyCustomization.sh
+++ CUSTOM_KEYBOARD/applyCustomization.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-sudo rm /usr/share/X11/xkb/symbols/it; sudo cp /home/tubbadu/code/GitHub/tubbadu/CUSTOM_KEYBOARD/it /usr/share/X11/xkb/symbols/it
+sudo rm /usr/share/X11/xkb/symbols/it; sudo cp /home/tubbadu/code/tubbadu/CUSTOM_KEYBOARD/it /usr/share/X11/xkb/symbols/it
